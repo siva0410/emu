285be787490341f8fa1e1b3812bae5d662d9e852
siva0410 <42541197+siva0410@users.noreply.github.com>
2021-12-28 00:02:07 +0900

    move ppu_reg from ppu to bus

--- bus/bus.go
+++ bus/bus.go
@@ -44,3 +44,127 @@ var PRG_ROM_ADDR uint16 = 0x8000
 var PPU_MEM [0x3FFF]byte
 var CHR_ROM_ADDR uint16 = 0x0000
 var PPU_PTR uint32
+
+/*
+   |-------------+---------+-----------+------------------------------------------------------------------|
+   | Common Name | Address | Bits      | Notes                                                            |
+   |-------------+---------+-----------+------------------------------------------------------------------|
+   | PPUCTRL     | %2000   | VPHB SINN | follow table                                                     |
+   | PPUMASK     | %2001   | BGRs bMmG | follow table                                                     |
+   | PPUSTATUS   | %2002   | VSO- ---- | follow table                                                     |
+   | OAMADDR     | %2003   | aaaa aaaa | OAM read/write address                                           |
+   | OAMDATA     | %2004   | dddd dddd | OAM data read/write                                              |
+   | PPUSCROLL   | %2005   | xxxx xxxx | fine scroll position (two writes: X scroll, Y scroll)            |
+   | PPUADDR     | %2006   | aaaa aaaa | PPU read/write address (two writes: most/least significant byte) |
+   | PPUDATA     | %2007   | dddd dddd | PPU data read/write                                              |
+   | OAMDMA      | %4014   | aaaa aaaa | OAM DMA high address                                             |
+   |-------------+---------+-----------+------------------------------------------------------------------|
+*/
+type PpuRegister struct {
+	Ppuctrl   byte // mode:W
+	Ppumask   byte // mode:W
+	Ppustatus byte // mode:R
+	Oamaddr   byte // mode:W
+	Oamdata   byte // mode:R/W
+	Ppuscroll byte // mode:W
+	Ppuaddr   byte // mode:W
+	Ppudata   byte // mode:R/W
+}
+
+var Ppu_reg, Before_ppu_reg *PpuRegister
+
+/*
+   |----------------------------+-----|
+   | PPUCTRL                    | bit |
+   |----------------------------+-----|
+   | NMI enable (V)             |   7 |
+   | PPU master/slave (P)       |   6 |
+   | sprite height (H)          |   5 |
+   | background tile select (B) |   4 |
+   | sprite tile select (S)     |   3 |
+   | increment mode (I)         |   2 |
+   | nametable select (NN)      | 1-0 |
+   |----------------------------+-----|
+*/
+func GetPpuCtrl(flagname string) bool {
+	var status byte
+	switch flagname {
+	case "NM1":
+		status = Ppu_reg.Ppuctrl >> 0 & 0b1
+	case "NM2":
+		status = Ppu_reg.Ppuctrl >> 1 & 0b1
+	case "I":
+		status = Ppu_reg.Ppuctrl >> 2 & 0b1
+	case "S":
+		status = Ppu_reg.Ppuctrl >> 3 & 0b1
+	case "B":
+		status = Ppu_reg.Ppuctrl >> 4 & 0b1
+	case "H":
+		status = Ppu_reg.Ppuctrl >> 5 & 0b1
+	case "P":
+		status = Ppu_reg.Ppuctrl >> 6 & 0b1
+	case "V":
+		status = Ppu_reg.Ppuctrl >> 7 & 0b1
+	}
+
+	var res bool = false
+	if status == 1 {
+		res = true
+	}
+
+	return res
+}
+
+/*
+   |-----------------------------------+-----|
+   | PPUMASK                           | bit |
+   |-----------------------------------+-----|
+   | color emphasis (R)                |   7 |
+   | color emphasis (G)                |   6 |
+   | color emphasis (B)                |   5 |
+   | sprite enable (s)                 |   4 |
+   | background enable (b)             |   3 |
+   | sprite left column enable (M)     |   2 |
+   | background left column enable (m) |   1 |
+   | greyscale (G)                     |   0 |
+   |-----------------------------------+-----|
+*/
+func GetPpuMask(flagname string) bool {
+	var status byte
+	switch flagname {
+	case "G":
+		status = Ppu_reg.Ppumask >> 0 & 0b1
+	case "m":
+		status = Ppu_reg.Ppumask >> 1 & 0b1
+	case "M":
+		status = Ppu_reg.Ppumask >> 2 & 0b1
+	case "b":
+		status = Ppu_reg.Ppumask >> 3 & 0b1
+	case "s":
+		status = Ppu_reg.Ppumask >> 4 & 0b1
+	case "RGB_B":
+		status = Ppu_reg.Ppumask >> 5 & 0b1
+	case "RGB_G":
+		status = Ppu_reg.Ppumask >> 6 & 0b1
+	case "RGB_R":
+		status = Ppu_reg.Ppumask >> 7 & 0b1
+	}
+
+	var res bool = false
+	if status == 1 {
+		res = true
+	}
+
+	return res
+}
+
+/*
+   |-----------------------------------------+-----|
+   | PPUSTATUS                               | bit |
+   |-----------------------------------------+-----|
+   | vblank (V)                              |   7 |
+   | sprite 0 hit (S)                        |   6 |
+   | sprite overflow (O)                     |   5 |
+   | read resets write pair for %2005/%2006  |   4 |
+   |-----------------------------------------+-----|
+*/
--- ppu/ppu.go
+++ ppu/ppu.go
@@ -5,212 +5,75 @@ import (
 	"fmt"
 )
 
-/*
-   |-------------+---------+-----------+------------------------------------------------------------------|
-   | Common Name | Address | Bits      | Notes                                                            |
-   |-------------+---------+-----------+------------------------------------------------------------------|
-   | PPUCTRL     | %2000   | VPHB SINN | follow table                                                     |
-   | PPUMASK     | %2001   | BGRs bMmG | follow table                                                     |
-   | PPUSTATUS   | %2002   | VSO- ---- | follow table                                                     |
-   | OAMADDR     | %2003   | aaaa aaaa | OAM read/write address                                           |
-   | OAMDATA     | %2004   | dddd dddd | OAM data read/write                                              |
-   | PPUSCROLL   | %2005   | xxxx xxxx | fine scroll position (two writes: X scroll, Y scroll)            |
-   | PPUADDR     | %2006   | aaaa aaaa | PPU read/write address (two writes: most/least significant byte) |
-   | PPUDATA     | %2007   | dddd dddd | PPU data read/write                                              |
-   | OAMDMA      | %4014   | aaaa aaaa | OAM DMA high address                                             |
-   |-------------+---------+-----------+------------------------------------------------------------------|
-*/
-type PpuRegister struct {
-	ppuctrl   byte // mode:W
-	ppumask   byte // mode:W
-	ppustatus byte // mode:R
-	oamaddr   byte // mode:W
-	oamdata   byte // mode:R/W
-	ppuscroll byte // mode:W
-	ppuaddr   byte // mode:W
-	ppudata   byte // mode:R/W
-}
-
-var ppu_reg, before_ppu_reg *PpuRegister
-var is_ppu_ptr_read bool = false
-
-/*
-   |----------------------------+-----|
-   | PPUCTRL                    | bit |
-   |----------------------------+-----|
-   | NMI enable (V)             |   7 |
-   | PPU master/slave (P)       |   6 |
-   | sprite height (H)          |   5 |
-   | background tile select (B) |   4 |
-   | sprite tile select (S)     |   3 |
-   | increment mode (I)         |   2 |
-   | nametable select (NN)      | 1-0 |
-   |----------------------------+-----|
-*/
-func getPpuCtrl(flagname string) bool {
-	var status byte
-	switch flagname {
-	case "NM1":
-		status = ppu_reg.ppuctrl >> 0 & 0b1
-	case "NM2":
-		status = ppu_reg.ppuctrl >> 1 & 0b1
-	case "I":
-		status = ppu_reg.ppuctrl >> 2 & 0b1
-	case "S":
-		status = ppu_reg.ppuctrl >> 3 & 0b1
-	case "B":
-		status = ppu_reg.ppuctrl >> 4 & 0b1
-	case "H":
-		status = ppu_reg.ppuctrl >> 5 & 0b1
-	case "P":
-		status = ppu_reg.ppuctrl >> 6 & 0b1
-	case "V":
-		status = ppu_reg.ppuctrl >> 7 & 0b1
-	}
-
-	var res bool = false
-	if status == 1 {
-		res = true
-	}
+func initPpuRegisters() *bus.PpuRegister {
+	res := new(bus.PpuRegister)
+	res.Ppuctrl = bus.CPU_MEM[0x2000]
+	res.Ppumask = bus.CPU_MEM[0x2001]
+	res.Ppustatus = bus.CPU_MEM[0x2002]
+	res.Oamaddr = bus.CPU_MEM[0x2003]
+	res.Oamdata = bus.CPU_MEM[0x2004]
+	res.Ppuscroll = bus.CPU_MEM[0x2005]
+	res.Ppuaddr = bus.CPU_MEM[0x2006]
+	res.Ppudata = bus.CPU_MEM[0x2007]
 
 	return res
 }
 
-/*
-   |-----------------------------------+-----|
-   | PPUMASK                           | bit |
-   |-----------------------------------+-----|
-   | color emphasis (R)                |   7 |
-   | color emphasis (G)                |   6 |
-   | color emphasis (B)                |   5 |
-   | sprite enable (s)                 |   4 |
-   | background enable (b)             |   3 |
-   | sprite left column enable (M)     |   2 |
-   | background left column enable (m) |   1 |
-   | greyscale (G)                     |   0 |
-   |-----------------------------------+-----|
-*/
-func getPpuMask(flagname string) bool {
-	var status byte
-	switch flagname {
-	case "G":
-		status = ppu_reg.ppumask >> 0 & 0b1
-	case "m":
-		status = ppu_reg.ppumask >> 1 & 0b1
-	case "M":
-		status = ppu_reg.ppumask >> 2 & 0b1
-	case "b":
-		status = ppu_reg.ppumask >> 3 & 0b1
-	case "s":
-		status = ppu_reg.ppumask >> 4 & 0b1
-	case "RGB_B":
-		status = ppu_reg.ppumask >> 5 & 0b1
-	case "RGB_G":
-		status = ppu_reg.ppumask >> 6 & 0b1
-	case "RGB_R":
-		status = ppu_reg.ppumask >> 7 & 0b1
-	}
-
-	var res bool = false
-	if status == 1 {
-		res = true
-	}
-
-	return res
+func fetchPpuRegisters(reg *bus.PpuRegister) {
+	reg.Ppuctrl = bus.CPU_MEM[0x2000]
+	reg.Ppumask = bus.CPU_MEM[0x2001]
+	reg.Ppustatus = bus.CPU_MEM[0x2002]
+	reg.Oamaddr = bus.CPU_MEM[0x2003]
+	reg.Oamdata = bus.CPU_MEM[0x2004]
+	reg.Ppuscroll = bus.CPU_MEM[0x2005]
+	reg.Ppuaddr = bus.CPU_MEM[0x2006]
+	reg.Ppudata = bus.CPU_MEM[0x2007]
 }
 
-/*
-   |-----------------------------------------+-----|
-   | PPUSTATUS                               | bit |
-   |-----------------------------------------+-----|
-   | vblank (V)                              |   7 |
-   | sprite 0 hit (S)                        |   6 |
-   | sprite overflow (O)                     |   5 |
-   | read resets write pair for %2005/%2006  |   4 |
-   |-----------------------------------------+-----|
-*/
-
-func initPpuRegisters() *PpuRegister {
-	res := new(PpuRegister)
-	res.ppuctrl = bus.CPU_MEM[0x2000]
-	res.ppumask = bus.CPU_MEM[0x2001]
-	res.ppustatus = bus.CPU_MEM[0x2002]
-	res.oamaddr = bus.CPU_MEM[0x2003]
-	res.oamdata = bus.CPU_MEM[0x2004]
-	res.ppuscroll = bus.CPU_MEM[0x2005]
-	res.ppuaddr = bus.CPU_MEM[0x2006]
-	res.ppudata = bus.CPU_MEM[0x2007]
-
-	return res
-}
-
-func fetchPpuRegisters(reg *PpuRegister) {
-	reg.ppuctrl = bus.CPU_MEM[0x2000]
-	reg.ppumask = bus.CPU_MEM[0x2001]
-	reg.ppustatus = bus.CPU_MEM[0x2002]
-	reg.oamaddr = bus.CPU_MEM[0x2003]
-	reg.oamdata = bus.CPU_MEM[0x2004]
-	reg.ppuscroll = bus.CPU_MEM[0x2005]
-	reg.ppuaddr = bus.CPU_MEM[0x2006]
-	reg.ppudata = bus.CPU_MEM[0x2007]
-}
-
-func copyPpuRegisters(before_reg, reg *PpuRegister) {
-	before_reg.ppuctrl = reg.ppuctrl
-	before_reg.ppumask = reg.ppumask
-	before_reg.ppustatus = reg.ppustatus
-	before_reg.oamaddr = reg.oamaddr
-	before_reg.oamdata = reg.oamdata
-	before_reg.ppuscroll = reg.ppuscroll
-	before_reg.ppuaddr = reg.ppuaddr
-	before_reg.ppudata = reg.ppudata
+func copyPpuRegisters(before_reg, reg *bus.PpuRegister) {
+	before_reg.Ppuctrl = reg.Ppuctrl
+	before_reg.Ppumask = reg.Ppumask
+	before_reg.Ppustatus = reg.Ppustatus
+	before_reg.Oamaddr = reg.Oamaddr
+	before_reg.Oamdata = reg.Oamdata
+	before_reg.Ppuscroll = reg.Ppuscroll
+	before_reg.Ppuaddr = reg.Ppuaddr
+	before_reg.Ppudata = reg.Ppudata
 }
 
 func InitPpu() {
-	ppu_reg = initPpuRegisters()
-	before_ppu_reg = initPpuRegisters()
+	bus.Ppu_reg = initPpuRegisters()
+	bus.Before_ppu_reg = initPpuRegisters()
 }
 
 func ExecPpu() {
-	fetchPpuRegisters(ppu_reg)
-	if before_ppu_reg.ppuaddr != ppu_reg.ppuaddr {
+	fetchPpuRegisters(bus.Ppu_reg)
+	if bus.Before_ppu_reg.Ppuaddr != bus.Ppu_reg.Ppuaddr {
 		bus.PPU_PTR = bus.PPU_PTR << 0x8
-		bus.PPU_PTR += uint32(ppu_reg.ppuaddr)
+		bus.PPU_PTR += uint32(bus.Ppu_reg.Ppuaddr)
 		bus.PPU_PTR &= 0xFFFF
 		fmt.Printf("MEM ppuaddr:%x\n\n", bus.PPU_PTR)
-		bus.PPU_MEM[bus.PPU_PTR] = ppu_reg.ppudata
-	}
-
-	// If ppuaddr is read, ppuaddr increments
-	if is_ppu_ptr_read {
-		if !getPpuCtrl("I") {
-			bus.PPU_PTR += 0x1
-		} else {
-			bus.PPU_PTR += 0x20
-		}
-		is_ppu_ptr_read = false
-	} else {
-		is_ppu_ptr_read = true
+		bus.PPU_MEM[bus.PPU_PTR] = bus.Ppu_reg.Ppudata
 	}
 
 	// backup ppu_reg to before_ppu_reg
-	copyPpuRegisters(before_ppu_reg, ppu_reg)
+	copyPpuRegisters(bus.Before_ppu_reg, bus.Ppu_reg)
 
 	// check ppu register
-	fmt.Printf("ppuctrl:%x\t", ppu_reg.ppuctrl)
-	fmt.Printf("ppustatus:%x\t", ppu_reg.ppustatus)
-	fmt.Printf("oamaddr:%x\t\n", ppu_reg.oamaddr)
-	fmt.Printf("oamdata:%x\t", ppu_reg.oamdata)
-	fmt.Printf("ppuaddr:%x\t", ppu_reg.ppuaddr)
-	fmt.Printf("ppudata:%x\t\n", ppu_reg.ppudata)
+	fmt.Printf("ppuctrl:%x\t", bus.Ppu_reg.Ppuctrl)
+	fmt.Printf("ppustatus:%x\t", bus.Ppu_reg.Ppustatus)
+	fmt.Printf("oamaddr:%x\t\n", bus.Ppu_reg.Oamaddr)
+	fmt.Printf("oamdata:%x\t", bus.Ppu_reg.Oamdata)
+	fmt.Printf("ppuaddr:%x\t", bus.Ppu_reg.Ppuaddr)
+	fmt.Printf("ppudata:%x\t\n", bus.Ppu_reg.Ppudata)
 	fmt.Printf("MEM ppuaddr:%x\n\n", bus.PPU_PTR)
 
-	fmt.Printf("before_ppuctrl:%x\t", before_ppu_reg.ppuctrl)
-	fmt.Printf("before_ppustatus:%x\t", before_ppu_reg.ppustatus)
-	fmt.Printf("before_oamaddr:%x\t\n", before_ppu_reg.oamaddr)
-	fmt.Printf("before_oamdata:%x\t", before_ppu_reg.oamdata)
-	fmt.Printf("before_ppuaddr:%x\t", before_ppu_reg.ppuaddr)
-	fmt.Printf("before_ppudata:%x\t\n\n", before_ppu_reg.ppudata)
+	fmt.Printf("before_ppuctrl:%x\t", bus.Before_ppu_reg.Ppuctrl)
+	fmt.Printf("before_ppustatus:%x\t", bus.Before_ppu_reg.Ppustatus)
+	fmt.Printf("before_oamaddr:%x\t\n", bus.Before_ppu_reg.Oamaddr)
+	fmt.Printf("before_oamdata:%x\t", bus.Before_ppu_reg.Oamdata)
+	fmt.Printf("before_ppuaddr:%x\t", bus.Before_ppu_reg.Ppuaddr)
+	fmt.Printf("before_ppudata:%x\t\n\n", bus.Before_ppu_reg.Ppudata)
 
 	// Palette()
 }
